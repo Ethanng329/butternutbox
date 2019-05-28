de35910fdc6cb19b82aea07a8a778e48ab540a34
Ethan Ng <ethanng329@gmail.com>
2019-05-28 11:37:02 +0100

    mock stub data

---
+++ src/CalenderData.js
@@ -0,0 +1,18 @@
+const CalendarData = [
+  { date: '27', dayOfWeek: 'Mon', free: true, month: 'May' },
+  { date: '28', dayOfWeek: 'Tue', free: false, month: 'May' },
+  { date: '29', dayOfWeek: 'Wed', free: true, month: 'May' },
+  { date: '30', dayOfWeek: 'Thurs', free: true, month: 'May' },
+  { date: '31', dayOfWeek: 'Fri', free: false, month: 'May' },
+  { date: '1', dayOfWeek: 'Sat', free: false, month: 'June' },
+  { date: '2', dayOfWeek: 'Sun', free: true, month: 'June' },
+  { date: '3', dayOfWeek: 'Mon', free: true, month: 'June' },
+  { date: '4', dayOfWeek: 'Tue', free: false, month: 'June' },
+  { date: '5', dayOfWeek: 'Wed', free: true, month: 'June' },
+  { date: '6', dayOfWeek: 'Thurs', free: true, month: 'June' },
+  { date: '7', dayOfWeek: 'Fri', free: false, month: 'June' },
+  { date: '8', dayOfWeek: 'Sat', free: false, month: 'June' },
+  { date: '9', dayOfWeek: 'Sun', free: true, month: 'June' }
+];
+
+export default CalendarData;
