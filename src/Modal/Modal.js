c97caec5c212d4dee1d0016cea4f8fae64463dc9
Ethan Ng <ethanng329@gmail.com>
2019-05-28 12:00:18 +0100

    add styling to selected date

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -6,6 +6,7 @@ function Modal({
   dateHandler,
   dayOfWeek,
   CalenderData,
+  selectedDate,
   monthRange
 }) {
   if (!show) {
@@ -16,33 +17,36 @@ function Modal({
     <div className="modal_container">
       <div className="modal_card">
         <p className="modal_card_text">{monthRange}</p>
-        <div className="grid_container">
-          {dayOfWeek.map((item, index) => {
-            return <p key={index}>{item}</p>;
-          })}
-        </div>
-
-        <div className="grid_container">
-          {CalenderData.map((item, index) => {
-            return (
-              <button
-                autoFocus
-                className={
-                  item === selectedDate ? 'date_button_selected' : 'date_button'
-                }
-                disabled={!item.free}
-                onClick={dateHandler}
-                key={index}
-                value={item.date}
-              >
-                {item.date}
-              </button>
-            );
-          })}
+        <div>
+          <div className="grid_container">
+            {dayOfWeek.map((item, index) => {
+              return <p key={index}>{item}</p>;
+            })}
+          </div>
+          <div className="grid_container">
+            {CalenderData.map((item, index) => {
+              return (
+                <button
+                  autoFocus
+                  className={
+                    item === selectedDate
+                      ? 'date_button_selected'
+                      : 'date_button'
+                  }
+                  disabled={!item.free}
+                  onClick={dateHandler}
+                  key={index}
+                  value={item.date}
+                >
+                  {item.date}
+                </button>
+              );
+            })}
+          </div>
         </div>
         <div className="button_container">
           <button className="date_update_button" onClick={onClick}>
-            <p>GOT IT</p>
+            GOT IT
           </button>
         </div>
       </div>
